ce4638c2d12fa1b72d42f320c69cd79f40c9d15c
Doug Young <dry@zestcash.com>
2012-04-07 19:41:10 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GmailAnalyzer::Application.config.secret_token = 'e23e7bd1c482ca66487604cb2a0f6614013d3b99f0cdf91e2f6ebd9d817e5d70d201b08fa41305436cafbca564371221494803ddf465bdf9b397fa8a0db25198'
