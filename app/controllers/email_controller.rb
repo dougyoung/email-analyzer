d0ed0c98e28fc4c841f6ab5f07ffb5bd79865075
Doug Young <dry@zestcash.com>
2012-04-07 23:41:21 -0700

    Added prototype HTML output to controller.

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,15 +1,27 @@
 require 'gmail'
 
 class EmailController < AuthenticationController
-  respond_to :json
 
   def index
-    senders_to_email_counts = senders_to_email_counts()
-    senders_to_email_counts = sort(senders_to_email_counts)
-    respond_with(senders_to_email_counts.as_json)
+    @senders_to_email_counts = senders_to_email_counts()
+    @senders_to_email_counts = sort(senders_to_email_counts)
+    @total_count = total_count
+    respond_to do |format|
+      format.html
+      format.json { render :json => @senders_to_email_counts.as_json }
+    end
   end
 
   private
+
+  def from(sender)
+    from = ""
+    unless sender.name.blank? || sender.name.downcase == "#{sender.mailbox.downcase}@#{sender.host.downcase}"
+      from = "#{sender.name} "
+    end
+    from += "<#{sender.mailbox.downcase}@#{sender.host.downcase}>"
+    from
+  end
 
   def senders_to_email_counts
     senders_to_email_counts = {}
@@ -29,17 +41,12 @@ class EmailController < AuthenticationController
     senders_to_email_counts
   end
 
-  def from(sender)
-    from = ""
-    unless sender.name.blank? || sender.name == "#{sender.mailbox}@#{sender.host}"
-      from = "#{sender.name} "
-    end
-    from += "<#{sender.mailbox}@#{sender.host}>"
-    from
-  end
-
   def sort(senders_to_email_counts)
     ActiveSupport::OrderedHash[senders_to_email_counts.sort_by { |key, value| value }.reverse]
+  end
+
+  def total_count
+    @gmail.inbox.emails.count
   end
 
   def uids
